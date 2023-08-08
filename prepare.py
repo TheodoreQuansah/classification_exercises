e4d86a46ca08c72f111ea07654835d33a2d2a360
TheodoreQuansah <theodore.s.quansah@gmail.com>
2023-08-08 14:16:15 -0500

    fourth edit

--- prepare.py
+++ prepare.py
@@ -1,19 +1,29 @@
-def prep_iris(get_iris_data):
+import os
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from env import get_connection
+from acquire import get_iris_data, get_titanic_data, get_telco_data
 
+def prep_iris():
+    iris_df = get_iris_data()  
+    iris_df = iris_df.drop(columns=['species_id', 'measurement_id'])
+    iris_df = iris_df.rename(columns={'species_name': 'species'})
     return iris_df
 
-def prep_titanic(get_titanic_data):
-
+def prep_titanic():
+    titanic_df = get_titanic_data()
+    titanic_df = titanic_df.drop(columns=['passenger_id', 'pclass', 'embarked'])
     return titanic_df
 
-def prep_telco(get_telco_data):
-
+def prep_telco():
+    telco_df = get_telco_data()
+    telco_df = telco_df.drop(columns = ['contract_type_id', 'contract_type_id.1', 
+                         'internet_service_type_id', 'internet_service_type_id.1', 
+                         'payment_type_id.1', 'payment_type_id'])
     return telco_df
 
-def train_val_test(df, strat, seed = 42):
-
-    train, val_test = train_test_split(df, train_size = 0.7, random_state = seed, stratify = df[strat])
-
-    val, test = train_test_split(val_test, train_size = 0.5, random_state = seed, stratify = val_test[strat])
-
+def train_val_test(df, strat, seed=42):
+    train, val_test = train_test_split(df, train_size=0.7, random_state=seed, stratify=df[strat])
+    val, test = train_test_split(val_test, train_size=0.5, random_state=seed, stratify=val_test[strat])
     return train, val, test
+
